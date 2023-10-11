9a09cc0bd47f594d878b854669ef8a2b665222e1
Pranto Sheikh <prantosheikh834@gmail.com>
2023-10-11 18:46:12 +0600

    some unusable comment remove

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const { Server, } = require("socket.io");
-
-// const io = new Server(8000, {
-//   cors: true,
-// });
-
-// const emailToSocketIdMap = new Map();
-// const socketidToEmailMap = new Map();
-
-// io.on("connection", (socket) => {
-//   console.log(`Socket Connected`, socket.id);
-//   socket.on("room:join", (data) => {
-//     const { email, room } = data;
-//     // console.log(data);
-//     emailToSocketIdMap.set(email, socket.id);
-//     socketidToEmailMap.set(socket.id, email);
-//     io.to(room).emit("room:join", data);
-//   });
-// });
-
 const { Server } = require("socket.io");
 
 const io = new Server(8000, {
